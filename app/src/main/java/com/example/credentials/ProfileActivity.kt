f700c26e6b583a0dc4f834a8f8bed6a7efd1bee9
cyrellebalderosa <cyrelleannazul.balderosa@bicol-u.edu.ph>
2021-07-29 15:25:35 +0800

    save encrypted data to firestore and decrypted data on app

--- app/src/main/java/com/example/credentials/ProfileActivity.kt
+++ app/src/main/java/com/example/credentials/ProfileActivity.kt
@@ -1,18 +1,23 @@
 package com.example.credentials
 
-import android.app.ProgressDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Patterns
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Base64
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import com.example.credentials.databinding.ActivityProfileBinding
-import com.example.credentials.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
+
 
     //viewBinding
     private lateinit var binding: ActivityProfileBinding
@@ -22,6 +27,15 @@ class ProfileActivity : AppCompatActivity() {
 
     //FirebaseAuth
     private lateinit var firebaseAuth: FirebaseAuth
+
+
+    private lateinit var btnsave: Button
+    private lateinit var editappsite: EditText
+    private lateinit var editusername: EditText
+    private lateinit var editpass: EditText
+   // private lateinit var showbutton: Button
+    private lateinit var outputdata: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +48,7 @@ class ProfileActivity : AppCompatActivity() {
         actionBar.title = "Profile"
 
         //init firebase auth
-        firebaseAuth= FirebaseAuth.getInstance()
+        firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
 
         //handle click, logout
@@ -43,7 +57,159 @@ class ProfileActivity : AppCompatActivity() {
             checkUser()
         }
 
+
+        btnsave = findViewById(R.id.savetbtn1)
+        editappsite = findViewById(R.id.appsiteEt)
+        editusername = findViewById(R.id.emailusernameEt)
+        editpass = findViewById(R.id.passEt)
+       // showbutton = findViewById(R.id.showbtn1)
+        outputdata = findViewById(R.id.outputdata)
+
+
+
+
+
+        editusername.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(editusername.text.toString()).matches())
+                    btnsave.isEnabled = true
+                else {
+
+                    btnsave.isEnabled = false
+                    editusername.setError("Invalid Email")
+
+                }
+
+            }
+
+        })
+
+
+        btnsave.setOnClickListener {
+
+            startActivity(Intent(this, credentialForm::class.java)
+                    .putExtra("Apps and Sites", editappsite.text.toString())
+                    //.putExtra("Email/ Username", editusername.text.toString().trim())
+                    //.putExtra("Password", editpass.text.toString().trim())
+                    .putExtra("output", outputdata.text.toString())
+            )
+
+            var Username = editusername.text.toString().trim()
+            var Password = editpass.text.toString().trim()
+
+
+            if (Username.isEmpty()) {
+                Toast.makeText(applicationContext, "Please enter a username", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if (Password.isEmpty()) {
+                Toast.makeText(applicationContext, "Please enter a Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+          Username = encodeData(Username)
+            Password = encodeData(Password)
+
+            //shared preferences init
+            val sharedPreferences = getSharedPreferences("SharedPref", 0)
+
+            //getting the editor to edit data
+            val editor = sharedPreferences.edit()
+
+            //putting the data into the editor
+            editor.putString("editusername", Username)
+            editor.putString("editpass", Password)
+
+            //apply changes
+            editor.apply()
+
+            //commit the changes
+            editor.commit()
+
+
+
+
+
+            val nameEncoded = sharedPreferences.getString("editusername", null)
+            val passEncoded = sharedPreferences.getString("editpass", null)
+
+            val name = decodeData(nameEncoded)
+            val pass = decodeData(passEncoded)
+
+            //show data
+            outputdata.text = "Decoded username: $name \nDecoded password: $pass "
+
+            var enusername = nameEncoded.toString()
+            var enpass = passEncoded.toString()
+
+            saveFireStore(enusername, enpass)
+
+
+        }
+
+
+
+
+
+
     }
+
+    //function to decode data
+    private fun decodeData(name: String?): String {
+        val encoded = Base64.decode(name?.toByteArray(), Base64.DEFAULT)
+        return String(encoded)
+
+
+    }
+
+    //function to encode   data
+    private fun encodeData(name: String): String{
+        val encoded = Base64.encode(name.toByteArray(), Base64.DEFAULT)
+        return String(encoded)
+    }
+
+
+
+
+
+
+
+
+
+    fun saveFireStore(username: String, password: String) {
+        val db = FirebaseFirestore.getInstance()
+        val user: MutableMap<String, Any> = HashMap()
+        user["Username"] = username
+        user["Password"] = password
+
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener {
+                    Toast.makeText(this@ProfileActivity, "record added succesfully", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this@ProfileActivity, "record failed to add", Toast.LENGTH_SHORT).show()
+
+                }
+
+
+    }
+
+
+
+
+
+
+
 
     private fun checkUser() {
         //check user is logged in or not
@@ -56,8 +222,11 @@ class ProfileActivity : AppCompatActivity() {
         }
         else{
             //user is null, user is not logged in, go to login activity
-            startActivity(Intent(this,LoginActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
     }
+
+
+
 }
--- app/src/main/java/com/example/credentials/credentialForm.kt
+++ app/src/main/java/com/example/credentials/credentialForm.kt
@@ -2,10 +2,47 @@ package com.example.credentials
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import com.example.credentials.databinding.ActivityCredentialFormBinding
+import com.example.credentials.databinding.ActivityProfileBinding
 
 class credentialForm : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCredentialFormBinding
+
+    private lateinit var textappsite: TextView
+    //private lateinit var textusername: TextView
+    //private lateinit var textpass: TextView
+    private lateinit var textoutput: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_credential_form)
+        binding = ActivityCredentialFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+        textappsite = findViewById(R.id.outputTv1)
+        //textusername = findViewById(R.id.outputTv2)
+        //textpass = findViewById(R.id.outputTv3)
+        textoutput = findViewById(R.id.TvOutputofshowbtn)
+
+        val appsite = intent.getStringExtra("Apps and Sites")
+        //val username = intent.getStringExtra("Email/ Username")
+       // val password = intent.getStringExtra("Password")
+        val output = intent.getStringExtra("output")
+
+
+        textappsite.text = "Apps and Sites: \n "+appsite
+       // textusername.text = "Email/ Username: "+username
+       // textpass.text = "Passwod: "+password
+        textoutput.text ="\n\n" +output
+
+
+
+
+
     }
 }
